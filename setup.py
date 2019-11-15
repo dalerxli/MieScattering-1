018e1aad7299adfe62b6cb5e1ae7fadc0405ec7a
nuno <nunodsousa@gmail.com>
2019-11-15 10:21:26 +0100

    Update of the setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='mole_mie',
-      version='0.1',
+      version='0.2',
       description='First version of a Mie Calculator',
       url='https://github.com/nunodsousa/MieScattering',
       author='Nuno de Sousa',
       author_email='nunodsousa@dipc.org',
       license='MIT',
       packages=['mole_mie'],
+      install_requires=['termcolor', 'numpy', 'pandas', 'scipy>=1.3.1']
       zip_safe=False)

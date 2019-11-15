4d8fc3aca6a6aaeae5e30db4c01668baa13e02cd
nuno <nunodsousa@gmail.com>
2019-11-15 21:21:20 +0100

    Inclusion of the coefficients

--- mole_mie/MieScatt.py
+++ mole_mie/MieScatt.py
@@ -35,10 +35,18 @@ class MieScatt(SpecialFunctions):
     mt = None
     N_multipoles = None
 
+    coeffs = None
+    expanded_Qext = None
+    cross_sections = None
+
     def __init__(self):
         self.showinfo()
 
     def showinfo(self):
+        """
+
+        :return:
+        """
         print(colored(50 * '__', 'blue'))
         print(colored('Mie Scattering program Initiated.\n', 'blue'))
         print('You are using {}. Please cite the following:'.format(colored('MoLE_Mie', 'red')))
@@ -49,6 +57,13 @@ class MieScatt(SpecialFunctions):
         print(colored(50*'__', 'blue'))
 
     def set_params(self, radius=None, medium_ref_index=None, N_multipoles=None):
+        """
+
+        :param radius:
+        :param medium_ref_index:
+        :param N_multipoles:
+        :return:
+        """
         self.a = radius;
         self.m = medium_ref_index;
         self.N_multipoles = N_multipoles;
@@ -58,6 +73,15 @@ class MieScatt(SpecialFunctions):
         print("Number of multipoles = ", self.N_multipoles)
 
     def compute_coeffs(self, mt, m, mp, alpha, beta):
+        """
+
+        :param mt:
+        :param m:
+        :param mp:
+        :param alpha:
+        :param beta:
+        :return:
+        """
         an = (mt * self.diff_psi(mp, alpha) * self.psi(mp, beta) - m * self.psi(mp, alpha) * self.diff_psi(mp,
                                                                                                            beta)) / (
                          mt * self.diff_xi(mp, alpha) * self.psi(mp, beta) - m * self.xi(mp, alpha) * self.diff_psi(
@@ -77,6 +101,12 @@ class MieScatt(SpecialFunctions):
         return an, bn, cn, dn
 
     def scan_cross_sections(self, wavelength_list, material):
+        """
+
+        :param wavelength_list:
+        :param material:
+        :return:
+        """
 
         self.check_parameters()
 
@@ -89,6 +119,12 @@ class MieScatt(SpecialFunctions):
         result = []
         result_Qexta_expanded = []
         result_Qextb_expanded = []
+
+        result_coeff_an = []
+        result_coeff_bn = []
+        result_coeff_cn = []
+        result_coeff_dn = []
+
 
         for wavelength in wavelength_list:
             mt = material.refractive_index(wavelength);
@@ -112,12 +148,28 @@ class MieScatt(SpecialFunctions):
             result_Qexta_expanded.append(Qext_a)
             result_Qextb_expanded.append(Qext_b)
 
+            result_coeff_an.append(an)
+            result_coeff_bn.append(bn)
+            result_coeff_cn.append(cn)
+            result_coeff_dn.append(dn)
+
         result = np.array(result)
 
         col_names_a = ['Qext_a' + str(i) for i in range(1,N_multipoles + 1)]
         col_names_b = ['Qext_b' + str(i) for i in range(1, N_multipoles + 1)]
+
+        col_coeff_a = ['a' + str(i) for i in range(1, N_multipoles + 1)]
+        col_coeff_b = ['b' + str(i) for i in range(1, N_multipoles + 1)]
+        col_coeff_c = ['c' + str(i) for i in range(1, N_multipoles + 1)]
+        col_coeff_d = ['d' + str(i) for i in range(1, N_multipoles + 1)]
+
         result_Qexta_expanded = pd.DataFrame(np.array(result_Qexta_expanded), columns=col_names_a)
         result_Qextb_expanded = pd.DataFrame(np.array(result_Qextb_expanded), columns=col_names_b)
+
+        result_coeff_an = pd.DataFrame(np.array(result_coeff_an), columns=col_coeff_a)
+        result_coeff_bn = pd.DataFrame(np.array(result_coeff_bn), columns=col_coeff_b)
+        result_coeff_cn = pd.DataFrame(np.array(result_coeff_cn), columns=col_coeff_c)
+        result_coeff_dn = pd.DataFrame(np.array(result_coeff_dn), columns=col_coeff_d)
 
         Qscat = np.sum(np.stack(list(result[:, 1])), axis=1)
         Qext = np.sum(np.stack(list(result[:, 2])), axis=1)
@@ -131,11 +183,19 @@ class MieScatt(SpecialFunctions):
         result_Qextb_expanded.index = wavelength_list
 
         self.expanded_Qext = pd.concat([result_Qexta_expanded, result_Qextb_expanded], axis = 1, sort = False)
-        
+
+        self.coeffs = pd.concat([result_coeff_an, result_coeff_bn, result_coeff_cn, result_coeff_dn], axis = 1, sort = False)
+        self.coeffs.index = wavelength_list
+
         self.cross_sections = pd.DataFrame([self.Qabs, self.Qext, self.Qscat]).T.rename(columns = {0:'Qabs', 1:'Qext', 2: 'Qscat'})
         self.cross_sections.index = wavelength_list
 
     def check_parameters(self, *arg):
+        """
+
+        :param arg:
+        :return:
+        """
         if (self.a == None):
             raise ValueError('The particle Radius is not defined.')
         if (self.m == None):

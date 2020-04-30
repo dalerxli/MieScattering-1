ff0dd6c31d7dbd6c215afb51c1a1b61c964231bc
nuno <nunodsousa@gmail.com>
2020-04-30 16:00:27 +0200

    Correction of the k/omega factor in the magnetic field.

--- mole_mie/MieScatt.py
+++ mole_mie/MieScatt.py
@@ -128,7 +128,7 @@ class MieScatt(SpecialFunctions):
     expanded_Qext = None
     cross_sections = None
 
-    c_const = 3E17; #(nm/s)
+    c_const = 1; #(nm/s)
 
     def __init__(self, silence=False):
         """
